459825054b780d6572e324cc1e8ca5b9e5db60a3
harijanaanesh026-dot <harijanaanesh026@gmail.com>
2026-08-09 21:25:55 +0530

    Update Firebase configuration in index.js
    Replaced Firebase configuration with hardcoded values.

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,15 @@ import { Home, Search, Target, Flame, MessageCircle, User, Bell, Plus, Send, Dol
 import toast, { Toaster } from "react-hot-toast";
 
 // ============ FIREBASE ============
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAT91pRDQrvCzxJHzhuzZe21K06xDy0sQ4",
+  authDomain: "nexoraai-75ae2.firebaseapp.com",
+  projectId: "nexoraai-75ae2",
+  storageBucket: "nexoraai-75ae2.firebasestorage.app",
+  messagingSenderId: "173122711177",
+  appId: "1:173122711177:web:68e373598d110d80c1e058",
+  measurementId: "G-11Y8XF8MBC"
 };
 if (!getApps().length) initializeApp(firebaseConfig);
 const auth = getAuth();

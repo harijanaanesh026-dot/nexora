5097bcf2ceee1f8ebf6fe6e5532bae1d2a3c7b78
harijanaanesh026-dot <harijanaanesh026@gmail.com>
2026-08-10 07:42:31 +0530

    Update Firebase configuration settings

--- pages/firebaseConfig.ts
+++ pages/firebaseConfig.ts
@@ -2,14 +2,15 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyATP9R0Qy-C2zuHHuzZcX06Dy0sQ4",
-  authDomain: "nexora-7seoz.firebaseapp.com",
-  projectId: "nexora-7seoz",
-  storageBucket: "nexora-7seoz.appspot.com",
-  messagingSenderId: "775122117177",
-  appId: "1:775122117177:web:d867c80b1080b1ce058",
-  measurementId: "G-11YEXFBWC"
+  apiKey: "AIzaSyAT91pRDQrvCzxJHzhuzZe21K06xDy0sQ4",
+  authDomain: "nexoraai-75ae2.firebaseapp.com",
+  projectId: "nexoraai-75ae2",
+  storageBucket: "nexoraai-75ae2.firebasestorage.app",
+  messagingSenderId: "173122711177",
+  appId: "1:173122711177:web:68e373598d110d80c1e058",
+  measurementId: "G-11Y8XF8MBC"
 };
 
 const app = initializeApp(firebaseConfig);

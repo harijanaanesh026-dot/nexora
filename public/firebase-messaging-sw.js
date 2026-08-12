2fa735e7a6258791b172e57a2e7819853552d39f
harijanaanesh026-dot <harijanaanesh026@gmail.com>
2026-08-12 22:14:23 +0530

    Add Firebase messaging service worker

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,23 @@
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAT91pRDQrvCzxJHzhuzZe21K06xDy0sQ4",
+  authDomain: "nexoraai-75ae2.firebaseapp.com",
+  projectId: "nexoraai-75ae2",
+  storageBucket: "nexoraai-75ae2.firebasestorage.app",
+  messagingSenderId: "173122711177",
+  appId: "1:173122711177:web:68e373598d110d80c1e058"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/favicon.ico'
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});

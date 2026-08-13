9e5a75a3cf2e3bb5632d085e68d25db08795844c
harijanaanesh026-dot <harijanaanesh026@gmail.com>
2026-08-13 23:16:41 +0530

    Refactor Firebase config to use environment variables

--- lib/firebaseConfig.ts
+++ lib/firebaseConfig.ts
@@ -1,18 +1,18 @@
-import { initializeApp } from 'firebase/app'
-import { getAuth } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
+import { getApps, initializeApp, getApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAT91pRDQrvCzxJHzhuzZe21K06xDy0sQ4",
-  authDomain: "nexoraai-75ae2.firebaseapp.com",
-  projectId: "nexoraai-75ae2",
-  storageBucket: "nexoraai-75ae2.firebasestorage.app",
-  messagingSenderId: "173122711177",
-  appId: "1:173122711177:web:68e373598d110d80c1e058",
-  measurementId: "G-11Y8XF8MBC"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
-const app = initializeApp(firebaseConfig);
+const app =!getApps().length? initializeApp(firebaseConfig) : getApp();
 export const auth = getAuth(app);
 export const db = getFirestore(app);
